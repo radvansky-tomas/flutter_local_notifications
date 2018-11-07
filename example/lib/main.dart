84b271719be68189e4fa85d3d88545a86959f0df
Tomas Radvansky <radvansky.tomas@gmail.com>
2018-11-07 20:22:51 +1300

    updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     var initializationSettingsAndroid =
         new AndroidInitializationSettings('app_icon');
-    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettingsIOS = new IOSInitializationSettings(registerUNUserNotificationCenterDelegate: false);
     var initializationSettings = new InitializationSettings(
         initializationSettingsAndroid, initializationSettingsIOS);
     flutterLocalNotificationsPlugin.initialize(initializationSettings,
